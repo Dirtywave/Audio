b7ee73616e1c4c6721c3f12ca45dff3dde5f7bea
timothy lamb <trash80@gmail.com>
2020-04-21 19:01:21 -0700

    Removal of conflicting audio library modules due to using Sdfat.

--- Audio.h
+++ Audio.h
@@ -119,8 +119,6 @@
 #include "output_adat.h"
 #include "play_memory.h"
 #include "play_queue.h"
-#include "play_sd_raw.h"
-#include "play_sd_wav.h"
 #include "play_serialflash_raw.h"
 #include "record_queue.h"
 #include "synth_tonesweep.h"
